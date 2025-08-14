c98e0e0ea3937cb2d516d02812596541ef583da5
developervinit <vinit91.dev@gmail.com>
2025-08-14 22:03:49 +0530

    Feat: make sidebar responsive align with the design

--- src/layouts/mainLayout/MainLayout.jsx
+++ src/layouts/mainLayout/MainLayout.jsx
@@ -1,21 +1,37 @@
-import { Outlet, NavLink } from 'react-router-dom';
-// import Header from '../components/common/header/Header';
-import styles from './MLayout.module.css'; // Import as CSS Module
+import { Outlet, NavLink } from "react-router-dom";
+import { useState } from "react";
+import styles from "./MLayout.module.css";
 
 const MainLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className={styles.layoutContainer}>
-      {/* <Header /> */}
-
       <div className={styles.mainLayout}>
-        <aside className={styles.sidebar}>
-          <NavLink to="/home" className={styles.navLink}>Create Resume</NavLink>
-          <NavLink to="/resumelist" className={styles.navLink}>My Resumes</NavLink>
-          <NavLink to="/" className={styles.navLink}>Log Out</NavLink>
+        <aside
+          className={`${styles.sidebar} ${
+            isSidebarOpen ? styles.sidebarOpen : ""
+          }`}
+        >
+          <button
+            className={styles.sidebarToggle}
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          >
+            ☰
+          </button>
+          <NavLink to="/home" className={styles.navLink}>
+            Create Resume
+          </NavLink>
+          <NavLink to="/resumelist" className={styles.navLink}>
+            My Resumes
+          </NavLink>
+          <NavLink to="/" className={styles.navLink}>
+            Log Out
+          </NavLink>
         </aside>
 
         <main className={styles.content}>
-          <Outlet /> {/* This will render child routes */}
+          <Outlet />
         </main>
       </div>
     </div>
